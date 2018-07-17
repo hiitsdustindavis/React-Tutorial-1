8a40f4df63357f04cc4a2fda2987c35e9ccebc68
Dustin Davis <hiitsdustindavis@gmail.com>
2018-07-16 23:12:31 -0700

    search bar component setup and git ignore

---
+++ src/components/search_bar.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+// The following function being stored in SearchBar is what's known as a Function Component in React. It's simple. It could even take in an argument and will return some JSX. For broader functionality we will use a Class Component.
+// const SearchBar = () => {
+//   return <input />;
+// };
+
+// The following is a Class Component refactoring of the Function Component above:
+// 'class' === Create a new class
+// 'SearchBar' === Name of new class
+// 'extends React.Component' === Make all of the React.Component Class functionalities available to the SearchBar Class
+
+// Every Class must have a render() function to return JSX to the DOM. Is render() a part of React.Component?
+
+// Handling Events in React has two steps:
+// 1. Declare Event Handler, which is a function/method that should he ran whenever the event occurs.
+// 2. Pass the Event Handler to the element that we want to monitor for events.
+// In this case we want to know whenever the input has its text changed.
+
+// <input onChange={this.onInputChange}/> is using the standard HTML input change event and passing it our function onInputChange. In other words when a user types into the input it will emit a change event that will trigger our function.
+
+// We will pass onInputChange one argument of 'event' which is standard Javascript event handling. Event is an object which carries very specific information about what changed. We can tap into (aka find its current value of) this object with dot notation.
+
+// STATE!
+// It is a plain Javascript Object that is used to record and react to user events
+// Each class based component that we define has its own state object. Whenever the components state is changed it immediately re-renders and also forces all of its children to re-render as well.
+// Before we ever use STATE inside of a class-based component we need to initialize the State Object. Function based components do not have state.
+
+// Constructor!
+// All Javascript Classes have a special function called "Constructor" The constructor function is the first and only one called automatically when a new instance of the class is created.
+// The constructor function is reserved for doing some setup inside of our Class like initializing variables, state, etc.
+// Each instance of a class-based compenent has it's own copy of state
+
+// Controlled component has its value set by state instead of its value telling state what to be. Example is the input value telling state to update.
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    //super() is allows us to call a parent method on the parent class. In this case the parent class is React.Component which has its own constructor function among many other functions.
+
+    this.state = { term: '' };
+    //Whenever we use state we initialize it by creating a new object and assigning it to "this.state" The object we pass will also contain properties that we want to record on the state. In this case we want to record the property "term"
+    // Everywhere else inside our components we will use the method this.setState() instead of this.state = 'something'
+  }
+
+  render() {
+    // return <input onChange={this.onInputChange}/>; REFACTOR below
+    return (
+      // So we pass the function this.setState and object that will set the term with the value of the input when the onChange event is triggered
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+      /* The value of the input is: {this.state.term}
+      We use "this.state.term" to reference the input value. It's ok here, becase we are not modifying it.
+      Also we wrap Javascript variables within JSX in {curly braces}.
+      */
+    );
+  }
+
+  // Code below was consolidated in the expression above
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,75 @@
+/*
+  ==========================
+    General Notes
+  ==========================
+  ### WHAT IS REACT: ###
+  React is a Javascript library that is used to produce HTML that is shown to a user in a web browser.
+  We are writing individual components, AKA "views"
+  Components: Nippits of code that produce HTML. So when you think "Component" or "View," think "Something that produces HTML"
+  When we write react code we write mulitple different components and we nest components within each other to make really complex applications relatively simple
+*/
+/*
+  ==========================
+    Application Notes
+  ==========================
+  1. What will this component do?
+     Create a component. This component should produce some HTML
+*/
+
+// Line below basically says go find React library from our Node Package installed in the folder called 'react'
+// This makes the react library available in this file.
+// 'React' is a variable representing the library
 import React from 'react';
+
+// To actually render something to the DOM we don't use the React Library. W
+// We use the ReactDOM library instead
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Here we import the SearchBar const from the search_bar.js file
+// When we include a file that we write ourselves we need to include a relative path for React to find it
+// We don't need to include a file path when importing packages like React and ReactDOM because the are namespaced (there is only one package for each)
+import SearchBar from './components/search_bar';
+require('./config');
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+
+// "const" is an ES6 (ES2016) piece of syntax - It's declaring a variable, but this is the final unchanging value. App will always have the same value.
+
+// const App = function() { -- We can use the "fat arrow" ES6 syntax to define a function. It seems that brevity is a possible theme of ES6? ¯\_(ツ)_/¯
+const App = () => {
+  // The "HTML" below is actually JSX which is a subset, or dialect of Javascript. It looks like HTML, but really behind the scenes it is just Javascript. JSX cannot be read by the browser. It represents the HTML that will be transpiled by Webpack and Babel before being compiled into a single JS file for the browser.
+  // When we write <div>Hi!</div> in JSX React calls createElement for us. Specifically it produces:
+    //  "React.createElement("div", null, "Hi!");"
+  // return <div>Hi!</div>;
+
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+}
+
+/*
+  2. Take this component's generated HTML and  put it into the DOM
+*/
+// - Learning Troubleshooting
+//  -- Attempt 1
+    // React.render(App);
+    // Result: "React is not defined" WE CANT USE React.render(); to render to the DOM
+
+    // We added the React import statment above which produced other errors
+    // After adding the React import statements above we get and error that says "Instead of passing a component class, make sure  to instantiate it by passing it to React. createElement."
+      // const App = function(){ return <div>Hi!</div>; } is actually a class. Think of it as a factory that produces instances that actually get rendered to the DOM. We must instantiate our compononents before we try to render them to the DOM
+    // When we write JSX and we put a component name like "div" in JSX as "<div></div>" the component name is actually a Component Class but using it inside of JSX it becomes a component instance.
+
+    // ReactDOM.render(App);
+    // Result: "React is not defined"
+
+    // By passing ReactDOM.render() "<App />" as an argument we are passing an instance of our component class App
+    // "<App />" is the short-hand self-closed version of "<App></App>"
+    // ReactDOM.render(<App />);
+    // This is almost right but you will get a console error of "Target container is not a DOM element"
+
+    // As you can tell from the error message above App does not know where to render. We need to tell React which element to render the component within. Often the Root Node of the application will be a div with the class '.container'
+    ReactDOM.render(<App />, document.querySelector('.container'));
